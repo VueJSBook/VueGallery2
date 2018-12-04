dae061678fe5a193920aeda872282d32c03f9b8e
Learning Laravel <support@learninglaravel.net>
2018-12-05 00:29:51 +0700

    Chapter 3

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -8,6 +8,7 @@
 <script>
     import ImageList from './ImageList'
     import SingleImage from './SingleImage'
+    import axios from 'axios'
 
     export default {
         name: 'gallery',
@@ -15,17 +16,18 @@
         data () {
             return {
                 selectedImage: '',
-                images: [
-                    {id: 1, title: 'First image', description: 'First image description', thumbnail: 'https://vuejsbook.com/img/vuejs/img1.jpg', imageLink: 'https://vuejsbook.com/img/vuejs/img1-l.jpg'},
-                    {id: 2, title: 'Second image', description: 'Second image description', thumbnail: 'https://vuejsbook.com/img/vuejs/img2.jpg', imageLink: 'https://vuejsbook.com/img/vuejs/img2-l.jpg'},
-                    {id: 3, title: 'Third image', description: 'Third image description', thumbnail: 'https://vuejsbook.com/img/vuejs/img3.jpg', imageLink: 'https://vuejsbook.com/img/vuejs/img3-l.jpg'},
-                    {id: 4, title: 'Fourth image', description: 'Fourth image description', thumbnail: 'https://vuejsbook.com/img/vuejs/img4.jpg', imageLink: 'https://vuejsbook.com/img/vuejs/img4-l.jpg'},
-                    {id: 5, title: 'Fifth image', description: 'Fifth image description', thumbnail: 'https://vuejsbook.com/img/vuejs/img5.jpg', imageLink: 'https://vuejsbook.com/img/vuejs/img5-l.jpg'},
-                    {id: 6, title: 'Sixth image', description: 'Sixth image description', thumbnail: 'https://vuejsbook.com/img/vuejs/img6.jpg', imageLink: 'https://vuejsbook.com/img/vuejs/img6-l.jpg'},
-                    {id: 7, title: 'Seventh image', description: 'Seventh image description', thumbnail: 'https://vuejsbook.com/img/vuejs/img7.jpg', imageLink: 'https://vuejsbook.com/img/vuejs/img7-l.jpg'},
-                    {id: 8, title: 'Eight image', description: 'Eighth image description', thumbnail: 'https://vuejsbook.com/img/vuejs/img8.jpg', imageLink: 'https://vuejsbook.com/img/vuejs/img8-l.jpg'}
-                ]
+                images: [],
+                errors: []
             }
+        },
+        created () {
+            axios.get('http://vuejsbook.test/api/v1/images')
+                .then(response => {
+                    this.images = response.data
+                })
+                .catch(e => {
+                    this.errors.push(e)
+                })
         },
         methods: {
             showImage: function (image) {
